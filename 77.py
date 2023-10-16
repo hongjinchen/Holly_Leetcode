a0386c534afe1b1acf458fcdb8ed7a5a5acef12b
HOLLY <71253179+hongjinchen@users.noreply.github.com>
2023-10-16 23:33:56 +0800

    更新美团面试问题

--- 77.py
+++ 77.py
@@ -8,9 +8,13 @@ class Solution:
         # 调用包解决问题
 
         # 回溯法
+        # 递归就可以用于解决多层嵌套循环的问题了
         res = []
         def backtrack(start=1, track=[]):
             if len(track) == k:
+                # 回溯函数终止条件
+                # 什么时候到达所谓的叶子节点了呢？
+                # path这个数组的大小如果达到k，说明我们找到了一个子集大小为k的组合了，在图中path存的就是根节点到叶子节点的路径。
                 res.append(track[:])
                 return
             for i in range(start, n+1):

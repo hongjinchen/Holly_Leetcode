3f76002139c9c983063233d279818015e2ffa8f6
HOLLY <71253179+hongjinchen@users.noreply.github.com>
2023-10-19 00:16:04 +0800

    美团无了5555

--- 14.py
+++ 14.py
@@ -1,7 +1,31 @@
-strs = ["flower","flow","flight"]
-minLength=0
-for item in strs:
-    if len(item)<minLength:
-        minLength=len(item)
-    else:
-        
+# 编写一个函数来查找字符串数组中的最长公共前缀。
+
+# 如果不存在公共前缀，返回空字符串 ""。
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        if not strs:
+            return ""
+        min_length = min([len(str) for str in strs])
+        if min_length==0:
+            return ""
+        ans=""
+        for i in range(min_length):
+            is_same = True
+            for j in range(len(strs)):
+                if strs[j][i]!=strs[0][i]:
+                    is_same=False
+            if is_same:
+                ans+=strs[0][i]
+            else:
+                break
+        return ans
+                    
+
+                    
+            
+
+
+if __name__=="__main__":
+    solution = Solution()
+    print(solution.longestCommonPrefix(["flower","flow","flight"]))
